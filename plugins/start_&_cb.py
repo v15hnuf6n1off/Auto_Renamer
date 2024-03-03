a33ffd7bd1fcc1bf5a490c0947f117414c6ef739
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-03-03 14:39:52 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -10,13 +10,13 @@ async def start(client, message):
     user = message.from_user
     await madflixbotz.add_user(client, message)                
     button = InlineKeyboardMarkup([[
-      InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-      InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+      InlineKeyboardButton('🔔 Updates', url='https://t.me/Mr_V_bots),
+      InlineKeyboardButton('🌐 Support', url='https://t.me/+MviX9PK66y4yZTBl')
     ],[
-      InlineKeyboardButton('⚙️ Help', callback_data='help'),
-      InlineKeyboardButton('💙 About', callback_data='about')
+      InlineKeyboardButton('❗️ Help', callback_data='help'),
+      InlineKeyboardButton('💡About', callback_data='about')
     ],[
-        InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url='https://t.me/CallAdminRobot')
+        InlineKeyboardButton("😎 Developer ", url='https://t.me/C0nt4ct_bot)
     ]])
     if Config.START_PIC:
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
@@ -33,13 +33,13 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-                InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+                InlineKeyboardButton('🔔 Updates', url='https://t.me/Mr_V_bots'),
+                InlineKeyboardButton('🌐 Support', url='https://t.me/MadflixBots_Support')
                 ],[
-                InlineKeyboardButton('⚙️ Help', callback_data='help'),
-                InlineKeyboardButton('💙 About', callback_data='about')
+                InlineKeyboardButton('❗️ Help', callback_data='help'),
+                InlineKeyboardButton('💡 About', callback_data='about')
                 ],[
-                InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url='https://t.me/CallAdminRobot')
+                InlineKeyboardButton("😎 Developer ", url='https://t.me/C0nt4ct_bot)
                 ]])
         )
     elif data == "caption":
