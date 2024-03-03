427300430262d82d28bafd5c88ae602009ad28b7
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-03-03 16:53:33 +0530

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -21,7 +21,7 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="🔺 Update Channel 🔺", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    buttons = [[InlineKeyboardButton(text="🔔 Update Channel🔔", url=f"https://t.me/{Config.FORCE_SUB}") ]]
     text = "<b>Hello Dear \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
